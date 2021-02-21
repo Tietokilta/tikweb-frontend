3cfabc4481272496bd79796b6284b2c3ef96d0a8
Valtteri Lakaniemi <valtteri.lakaniemi@gmail.com>
2021-02-21 14:12:04 +0200

    Fix issue with React import errors

---
+++ gatsby-node.js
@@ -0,0 +1,11 @@
+exports.onCreateBabelConfig = ({ actions }) => {
+  // This fixes errors when not importing React in situations where it's not
+  // necessary according to React 17.
+  // SEE: https://github.com/gatsbyjs/gatsby/issues/28657
+  actions.setBabelPlugin({
+    name: "@babel/plugin-transform-react-jsx",
+    options: {
+      runtime: "automatic",
+    },
+  })
+}
