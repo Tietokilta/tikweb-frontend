8ef46030d53753f50a176e1661cb01c1d6ab5a24
Ilmari Tarpila <leo.tarpila@aalto.fi>
2021-02-25 17:15:49 +0200

    Strapi integration init

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,15 @@ module.exports = {
   siteMetadata: {
     title: "Tietokilta",
   },
-  plugins: ["gatsby-plugin-styled-components"],
+  plugins: [
+    "gatsby-plugin-styled-components",
+    {
+      resolve: "gatsby-source-strapi",
+      options: {
+        apiURL: "http://localhost:1337",
+        contentTypes: ["restaurant", "category"],
+        queryLimit: 1000,
+      },
+    },
+  ],
 }
