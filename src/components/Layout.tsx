25fc4082ac732214094658ff3d26063411b0ea23
Valtteri Lakaniemi <valtteri.lakaniemi@gmail.com>
2021-03-07 14:44:27 +0200

    Make page full height

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,18 +1,28 @@
-import { ThemeProvider } from "styled-components"
+import styled, { ThemeProvider } from "styled-components"
 
 import GlobalStyle from "../styles/global-style"
 import theme from "../styles/theme"
 import Header from "./Header"
+
+const LayoutContainer = styled.div`
+  min-height: 100%;
+  display: flex;
+  flex-flow: column nowrap;
+`
+
+const PageContent = styled.main`
+  flex-grow: 1;
+`
 
 const Layout: React.FC = (props) => {
   const { children } = props
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <div>
+      <LayoutContainer>
         <Header />
-        <main>{children}</main>
-      </div>
+        <PageContent>{children}</PageContent>
+      </LayoutContainer>
     </ThemeProvider>
   )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,13 +3,11 @@ import Layout from "../components/Layout"
 const IndexPage: React.FC = () => {
   return (
     <Layout>
-      <main>
-        <h1>Hello, world! h1</h1>
-        <h2>Hello, world! h2</h2>
-        <h3>Hello, world! h3</h3>
-        <h4>Hello, world! h4</h4>
-        <div>Normal text</div>
-      </main>
+      <h1>Hello, world! h1</h1>
+      <h2>Hello, world! h2</h2>
+      <h3>Hello, world! h3</h3>
+      <h4>Hello, world! h4</h4>
+      <div>Normal text</div>
     </Layout>
   )
 }
