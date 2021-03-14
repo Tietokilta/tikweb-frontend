6be0dfc63b219d8daefd0089b2f4d6b85071f584
Valtteri Lakaniemi <valtteri.lakaniemi@gmail.com>
2021-03-14 14:11:51 +0200

    Change content-types for strapi data source

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,7 +8,7 @@ module.exports = {
       resolve: "gatsby-source-strapi",
       options: {
         apiURL: "http://localhost:1337",
-        contentTypes: ["restaurant", "category"],
+        contentTypes: ["page"],
         queryLimit: 1000,
       },
     },
