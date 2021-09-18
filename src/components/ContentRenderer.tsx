7d58e609b62b956e2bdc32ae44bae578299f70fb
Valtteri Lakaniemi <valtteri.lakaniemi@gmail.com>
2021-09-18 16:03:52 +0300

    Add missing key prop

--- src/components/ContentRenderer.tsx
+++ src/components/ContentRenderer.tsx
@@ -1,3 +1,4 @@
+import { v4 as uuid } from "uuid"
 import ReactMarkdown from "react-markdown"
 import { StrapiPageContentBlock } from "../types/strapi"
 
@@ -10,7 +11,7 @@ const ContentRenderer: React.FC<Props> = ({ contentBlocks }) => {
     <>
       {contentBlocks.map((contentBlock) => {
         if (contentBlock.strapi_component === "common-content.text-block") {
-          return <ReactMarkdown>{contentBlock.text}</ReactMarkdown>
+          return <ReactMarkdown key={uuid()}>{contentBlock.text}</ReactMarkdown>
         }
 
         return null
