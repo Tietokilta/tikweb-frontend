b9a59242e65592f7674f1220c3318ab68e9baa41
UrjalaCoder <myllymakituomas1998@gmail.com>
2021-11-01 14:42:13 +0200

    Progress

--- src/components/TextContainer.tsx
+++ src/components/TextContainer.tsx
@@ -1,23 +1,13 @@
-import { FullWidthContainer } from "../components/Containers"
-import Hero from "../components/Hero"
-import Layout from "../components/Layout"
-import { TextContainer } from "../components/TextContainer"
+import { FullWidthContainer } from "./Containers"
 
-const IndexPage: React.FC = () => {
-  return (
-    <Layout>
-      <Hero />
-      <div className="justify-center flex">
-        <TextContainer />
-        <FullWidthContainer>
+export const TextContainer: React.FC = () => {
+    return (
+        <FullWidthContainer className="relative">
             Tietokilta ry (TiK) on Aalto-yliopiston Perustieteiden korkeakoulun tietotekniikan koulutusohjelman opiskelijoiden ainejärjestö eli kilta, joka on avoin muillekin killan toiminnasta kiinnostuneille.
             Tietokilta järjestää opiskelijoilleen erilaista vapaa-ajan toimintaa niin Otaniemessä kuin sen ulkopuolellakin, valvoo heidän etujaan korkeakoulun toimielimissä, huolehtii yhteyksistä muihin opiskelijajärjestöihin ja pyrkii muuten huolehtimaan heidän hyvinvoinnistaan. Kaikki killan toiminta perustuu hyvien teekkariperinteiden mukaiseen vapaaehtoistyöhön ja yhteiseen talkoohenkeen.
             Kampuksella tietokiltalaiset löytää parhaiten kiltahuoneelta eli kiltikseltä, joka sijaitsee Tietotekniikan Talolla osoitteessa Konemiehentie 2 rakennuksen koilliskulmassa.
             TiK toimii kaikkien Aalto-yliopiston opiskelijoiden etuja ajavan Aalto-yliopiston ylioppilaskunnan (AYY) piirissä.
         </FullWidthContainer>
-      </div>
-    </Layout>
-  )
+    )
 }
-
-export default IndexPage
+  
