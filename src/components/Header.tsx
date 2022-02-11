8abd85b20fbc32dea4111602645bcf08fe693924
Valtteri Lakaniemi <valtteri.lakaniemi@gmail.com>
2022-02-11 18:52:59 +0200

    Make header logo clickable, make header sticky

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,5 @@
+import { Link } from "gatsby"
+
 import NavBar from "./NavBar"
 import logo from "../assets/logo.svg"
 import { FullWidthContainer } from "./Containers"
@@ -5,12 +7,15 @@ import { FullWidthContainer } from "./Containers"
 const Header: React.FC = () => {
   return (
     <>
-      <div className="flex justify-center w-full h-12 px-4 md:px-8 lg:px-16 bg-black text-white">
+      <div className="sticky top-0 left-0 flex justify-center w-full h-12 px-4 md:px-8 lg:px-16 bg-black text-white">
         <FullWidthContainer className="flex justify-between">
-          <div className="flex items-center text-2xl">
+          <Link
+            to="/"
+            className="sticky flex items-center font-normal text-2xl"
+          >
             <img className="w-8 h-8 mr-2" src={logo} alt="logo" />
             Tietokilta Ry
-          </div>
+          </Link>
           <NavBar />
         </FullWidthContainer>
       </div>
