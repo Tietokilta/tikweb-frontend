b8632ddaa370ca405f39a8ac811a25e37af815f3
Evan <2248392717@qq.com>
2022-10-23 17:23:21 +0300

    fix prettier

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,7 +7,7 @@ const Footer: React.FC = () => {
   return (
     <div className="flex w-full text-white justify-center bg-gray-darkest px-4 md:px-8 lg:px-16 py-6">
       <FullWidthContainer className="max-w-[90rem] flex flex-col items-center">
-        <p className='text-3xl pt-4 pb-16'>Tietokilta Main Partners</p>
+        <p className="text-3xl pt-4 pb-16">Tietokilta Main Partners</p>
         <div className="w-[60vw] flex mx-auto justify-center pb-40">
           <img src={Logo1} alt="Futurice" className="h-[5vw]" />
           <img src={Logo2} alt="Vector" className="h-[5vw] mx-[4vw]" />
