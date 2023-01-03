cb0c3c76e3ef5ab7773d1c8acb5310a20e2722a9
Petrus Asikainen <petrus.asikainen@aalto.fi>
2023-01-04 00:01:42 +0200

    Comment awaitProperties

--- src/utils/awaitProperties.ts
+++ src/utils/awaitProperties.ts
@@ -2,6 +2,9 @@ type AwaitedObject<T extends { [key: string]: unknown }> = {
   [key in keyof T]: Awaited<T[key]>
 }
 
+/** Returns a Promise that is resolved with a mapping of results when all the
+ * properties in the given object are awaited.
+ */
 export default async function awaitProperties<
   T extends { [key: string]: unknown }
 >(obj: T): Promise<AwaitedObject<T>> {
